abc44a35110cd6bbfd61ce8f445b5072e399a2ad
Sevenn <thomas.s833@gmail.com>
2016-11-24 11:18:02 +0100

    Getter et setter

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -16,4 +16,164 @@ class Client
     private $ville;
     private $tel;
     private $lesCommandes;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @param mixed $titre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdresserue1()
+    {
+        return $this->adresserue1;
+    }
+
+    /**
+     * @param mixed $adresserue1
+     */
+    public function setAdresserue1($adresserue1)
+    {
+        $this->adresserue1 = $adresserue1;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdresserue2()
+    {
+        return $this->adresserue2;
+    }
+
+    /**
+     * @param mixed $adresserue2
+     */
+    public function setAdresserue2($adresserue2)
+    {
+        $this->adresserue2 = $adresserue2;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCp()
+    {
+        return $this->cp;
+    }
+
+    /**
+     * @param mixed $cp
+     */
+    public function setCp($cp)
+    {
+        $this->cp = $cp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * @param mixed $ville
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * @param mixed $tel
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLesCommandes()
+    {
+        return $this->lesCommandes;
+    }
+
+    /**
+     * @param mixed $lesCommandes
+     */
+    public function setLesCommandes($lesCommandes)
+    {
+        $this->lesCommandes = $lesCommandes;
+    }
 }
--- src/Entity/Commande.php
+++ src/Entity/Commande.php
@@ -8,4 +8,52 @@ class Commande
     private $id;
     private $datecde;
     private $leClient;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDatecde()
+    {
+        return $this->datecde;
+    }
+
+    /**
+     * @param mixed $datecde
+     */
+    public function setDatecde($datecde)
+    {
+        $this->datecde = $datecde;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLeClient()
+    {
+        return $this->leClient;
+    }
+
+    /**
+     * @param mixed $leClient
+     */
+    public function setLeClient($leClient)
+    {
+        $this->leClient = $leClient;
+    }
 }
