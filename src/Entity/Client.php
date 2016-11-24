8ac5dc0fb548435f2f458aa8c24f6c6e625ea3f9
Sevenn <thomas.s833@gmail.com>
2016-11-24 08:57:13 +0100

    Ajout des deux class db

---
+++ src/Entity/Client.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace src\Entity;
+
+class Client
+{
+    private $id;
+    private $titre;
+    private $nom;
+    private $prenom;
+    private $adresserue1;
+    private $adresserue2;
+    private $cp;
+    private $ville;
+    private $tel;
+    private $lesCommandes;
+}
---
+++ src/Entity/Commande.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types = 1);
+
+namespace src\Entity;
+
+class Commande
+{
+    private $id;
+    private $datecde;
+    private $leClient;
+}
